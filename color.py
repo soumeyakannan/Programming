73e459c1b65df351d988a22911cd8cf63b997a28
soumeyakannan <soumeyakannan@gmail.com>
2024-03-30 23:44:26 +0530

    My first ever program in Python.

---
+++ color.py
@@ -0,0 +1,14 @@
+color = input("what is your favourite color?\n")
+if color == "pink":
+    print("me too!")
+    time = input("when did you start liking pink?\n")
+    if "childhood" in time:
+        print("omg samee!!")
+    else:
+        if "yuck" in time:
+            print("i thought we were friends:(")
+        else:
+            print("that's nice, i liked it since i was 4!")
+    
+else:
+    print("doofus!")
